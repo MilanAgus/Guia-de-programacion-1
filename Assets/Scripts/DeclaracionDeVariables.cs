8df00ac865801288bbdb9aa5dd3ad899c769681d
MilanAgus <167197440+MilanAgus@users.noreply.github.com>
2025-03-17 09:07:42 -0300

    PrimerasVariables

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    float horasDeViaje= 10.5;
+    int alumnosQueViajan= 25;
+    string destino= "La falda, Córdoba";
+    bool llegoATiempo= false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
